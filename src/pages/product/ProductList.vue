ee4a5620a98ab889a506cd2103f7929a86089ee7
kimi.lai <laidazhang@qq.com>
2019-03-20 23:44:29 +0800

    init

---
+++ src/pages/product/ProductList.vue
@@ -0,0 +1,189 @@
+<template>
+  <a-card>
+    <div :class="advanced ? 'search' : null">
+      <a-form layout="horizontal">
+        <div :class="advanced ? null: 'fold'">
+          <a-row >
+          <a-col :md="8" :sm="24" >
+            <a-form-item
+              label="商品ID"
+              :labelCol="{span: 5}"
+              :wrapperCol="{span: 18, offset: 1}"
+            >
+              <a-input placeholder="请输入" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24" >
+            <a-form-item
+              label="商品名称"
+              :labelCol="{span: 5}"
+              :wrapperCol="{span: 18, offset: 1}"
+            >
+            <a-input placeholder="请输入" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24" >
+            <a-form-item
+              label="商品分类"
+              :labelCol="{span: 5}"
+              :wrapperCol="{span: 18, offset: 1}"
+            >
+              <a-select placeholder="请选择">
+                <a-select-option value="1">水果</a-select-option>
+                <a-select-option value="2">海鲜干货</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+        </a-row>
+          <a-row v-if="advanced">
+          <a-col :md="8" :sm="24" >
+            <a-form-item
+              label="商品状态"
+              :labelCol="{span: 5}"
+              :wrapperCol="{span: 18, offset: 1}"
+            >
+              <a-select placeholder="请选择">
+                <a-select-option value="1">关闭</a-select-option>
+                <a-select-option value="2">运行中</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        </div>
+        <span style="float: right; margin-top: 3px;">
+          <a-button type="primary">查询</a-button>
+          <a-button style="margin-left: 8px">重置</a-button>
+          <a @click="toggleAdvanced" style="margin-left: 8px">
+            {{advanced ? '收起' : '展开'}}
+            <a-icon :type="advanced ? 'up' : 'down'" />
+          </a>
+        </span>
+      </a-form>
+    </div>
+    <div>
+      <div class="operator">
+        <a-button @click="addNew" type="primary">新建</a-button>
+        <a-button >批量操作</a-button>
+        <a-dropdown>
+          <a-menu @click="handleMenuClick" slot="overlay">
+            <a-menu-item key="delete">删除</a-menu-item>
+            <a-menu-item key="audit">审批</a-menu-item>
+          </a-menu>
+          <a-button>
+            更多操作 <a-icon type="down" />
+          </a-button>
+        </a-dropdown>
+      </div>
+      <standard-table
+        :columns="columns"
+        :dataSource="dataSource"
+        :selectedRows="selectedRows"
+        @change="onchange"
+      />
+    </div>
+  </a-card>
+</template>
+
+<script>
+import StandardTable from '../../components/table/StandardTable'
+import AInput from "ant-design-vue/es/input/Input";
+const columns = [
+  {
+    title: '商品编号',
+    dataIndex: 'id'
+  },
+  {
+    title: '商品名称',
+    dataIndex: 'title'
+  },
+  {
+    title: '商品库存',
+    dataIndex: 'storeQty',
+    sorter: true
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    needTotal: true
+  },
+  {
+    title: '上架时间',
+    dataIndex: 'createTime',
+    sorter: true
+  }
+]
+
+const dataSource = []
+
+for (let i = 0; i < 100; i++) {
+  dataSource.push({
+    key: i,
+    id: i,
+    title: '商品' + i,
+    storeQty: Math.floor(Math.random() * 1000),
+    status: Math.floor(Math.random() * 10) % 4,
+    createTime: new Date().toLocaleDateString()
+  })
+}
+
+export default {
+  name: 'ProductList',
+  components: {AInput, StandardTable},
+  data () {
+    return {
+      advanced: true,
+      columns: columns,
+      dataSource: dataSource,
+      selectedRowKeys: [],
+      selectedRows: []
+    }
+  },
+  methods: {
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+    },
+    onchange (selectedRowKeys, selectedRows) {
+      this.selectedRowKeys = selectedRowKeys
+      this.selectedRows = selectedRows
+    },
+    remove () {
+      this.dataSource = this.dataSource.filter(item => this.selectedRowKeys.indexOf(item.key) < 0)
+      this.selectedRows = this.selectedRows.filter(item => this.selectedRowKeys.indexOf(item.key) < 0)
+    },
+    addNew () {
+      alert('add new row' + new Date())
+      /*this.dataSource.unshift({
+        key: this.dataSource.length,
+        no: 'NO ' + this.dataSource.length,
+        description: '这是一段描述',
+        callNo: Math.floor(Math.random() * 1000),
+        status: Math.floor(Math.random() * 10) % 4,
+        updatedAt: '2018-07-26'
+      })*/
+    },
+    handleMenuClick (e) {
+      if (e.key === 'delete') {
+        this.remove()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .search{
+    margin-bottom: 54px;
+  }
+  .fold{
+    width: calc(100% - 216px);
+    display: inline-block
+  }
+  .operator{
+    margin-bottom: 18px;
+  }
+  @media screen and (max-width: 900px) {
+    .fold {
+      width: 100%;
+    }
+  }
+</style>
